cf7beab3fabc854540a8f0003f7fc270547a3212
Olivier Louvignes <olivier@mg-crea.com>
2017-08-19 18:46:48 +0200

    chore(import): add missing fetch export

--- src/helpers/fetch.js
+++ src/helpers/fetch.js
@@ -68,7 +68,7 @@ export const buildFetchOpts = ({context, contextOpts, actionOpts}) => {
   return opts;
 };
 
-const fetch = (url, options = {}) => {
+export const fetch = (url, options = {}) => {
   // Support options.query
   const builtUrl = Object.keys(options.query || []).reduce((wipUrl, queryParam) => {
     const queryParamValue = options.query[queryParam];
