7fd42087adfc22f62c9e6d13f3d9969315069c88
Olivier Louvignes <olivier@mg-crea.com>
2017-10-20 11:22:08 +0200

    feat(test): upgrade to a modern test stack

--- test/spec/index.spec.js
+++ test/spec/index.spec.js
@@ -8,7 +8,7 @@ const url = `${host}/users/:id`;
 
 describe('lib', () => {
   it('should properly export fetch', () => {
-    expect(fetch).toBeA('function');
+    expect(typeof fetch).toBe('function');
   });
 });
 
@@ -16,29 +16,29 @@ describe('createResource', () => {
   it('should throw if name is undefined', () => {
     expect(() => {
       const resource = createResource();
-      expect(resource).toBeA('object');
+      expect(typeof resource).toBe('object');
     }).toThrow();
   });
   it('should properly return an object with properly named keys', () => {
     const {types, actions, reducers} = createResource({name, url});
-    expect(types).toBeA('object');
-    expect(actions).toBeA('object');
-    expect(reducers).toBeA('function');
+    expect(typeof types).toBe('object');
+    expect(typeof actions).toBe('object');
+    expect(typeof reducers).toBe('function');
   });
   it('should properly merge action opts', () => {
     const {types, actions, reducers} = createResource({name, url, actions: {get: {foo: 'bar'}, charge: {method: 'post'}}});
-    expect(types).toBeA('object');
+    expect(typeof types).toBe('object');
     expect(Object.keys(types).length).toEqual(6);
-    expect(actions).toBeA('object');
+    expect(typeof actions).toBe('object');
     expect(Object.keys(actions).length).toEqual(6);
-    expect(reducers).toBeA('function');
+    expect(typeof reducers).toBe('function');
   });
   it('should properly pick action opts', () => {
     const {types, actions, reducers} = createResource({name, url, pick: ['fetch']});
-    expect(types).toBeA('object');
+    expect(typeof types).toBe('object');
     expect(Object.keys(types).length).toEqual(1);
-    expect(actions).toBeA('object');
+    expect(typeof actions).toBe('object');
     expect(Object.keys(actions).length).toEqual(1);
-    expect(reducers).toBeA('function');
+    expect(typeof reducers).toBe('function');
   });
 });
